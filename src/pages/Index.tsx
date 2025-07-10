b0de6be6a5421ab724e2bc3ebd98a868c7924538
unknown <kalloello@gmail.com>
2025-07-10 07:04:46 +0100

    changed to server

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -35,7 +35,8 @@ const Index = () => {
   const [timezone, setTimezone] = useState("");
 
   // const API_URL = "https://pancakeswapsignal.onrender.com/api/signals"; // Your backend API
-  const API_URL = "https://projectmlpancakeswap.onrender.com/api/signals"; // Your backend API
+  // const API_URL = "https://projectmlpancakeswap.onrender.com/api/signals"; // Your backend API
+  const API_URL = "http://188.165.71.103:3000"; // Your backend API
   // const API_URL = "https://pancake-signalsugragph.onrender.com/api/signals"; // Your backend API
 
   const fetchSignals = async () => {
